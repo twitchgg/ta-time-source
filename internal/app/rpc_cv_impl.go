8ffd5b7005cbcb9802d30e2bb654ae4c5a2b8e3a
TwitchGG <twitchgg@yahoo.com>
2022-07-22 13:13:52 +0800

    添加用户端共视数据推送gRPC实现

--- internal/app/rpc_cv_impl.go
+++ internal/app/rpc_cv_impl.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"strings"
+
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc/codes"
 	"ntsc.ac.cn/tas/tas-commons/pkg/pb"
@@ -21,8 +23,8 @@ func (tsa *TimeSourceApp) findUserCVSession(machineID string) *userCVSession {
 	return nil
 }
 
-func (tsa *TimeSourceApp) PushMainStationData(req *pb.PushRequest,
-	stream pb.CommonViewDataService_PushMainStationDataServer) error {
+func (tsa *TimeSourceApp) PushStationData(req *pb.PushRequest,
+	stream pb.CommonViewDataService_PushStationDataServer) error {
 	logrus.WithField("prefix", "app.cv").Infof(
 		"connect device [%s]", req.MachineID)
 	if err := rpc.CheckMachineID(stream.Context(), req.MachineID); err != nil {
@@ -44,4 +46,38 @@ func (tsa *TimeSourceApp) PushMainStationData(req *pb.PushRequest,
 		}
 	}
 	return nil
+}
+
+func (tsa *TimeSourceApp) PullStationData(
+	stream pb.CommonViewDataService_PullStationDataServer) error {
+	var machineID string
+	var err error
+	for {
+		if machineID == "" {
+			if machineID, err = rpc.GetMachineID(stream.Context()); err != nil {
+				return rpc.GenerateArgumentError("machine id")
+			}
+		}
+		rpcData, err := stream.Recv()
+		if err != nil {
+			if strings.Contains(err.Error(), "EOF") {
+				logrus.WithField("prefix", "app.cv").Infof(
+					"session by machine-id [%s] closed", machineID)
+				return nil
+			}
+			return rpc.GenerateError(codes.Canceled, err)
+		}
+		line := string(rpcData.Data)
+		if len(line) < 10 {
+			logrus.WithField("prefix", "service.cv").
+				Warnf("failed to parse user common view data [%s]", line)
+			continue
+		}
+		data := line[1 : len(line)-1]
+		lineData := strings.Split(data, ",")
+		if err := tsa.process(rpcData.Mode.String(), machineID, rpcData.Data, lineData); err != nil {
+			logrus.WithField("prefix", "service.cv").
+				Errorf("failed to save user common view data [%v]", err)
+		}
+	}
 }
